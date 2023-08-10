743ac94419f178de5a7c986c157643dd33eafcda
impath96 <impath96@gmail.com>
2023-08-10 15:49:10 +0900

    Initial commit

---
+++ src/main/java/com/member/invitation/memberinvitation/MemberInvitationApplication.java
@@ -0,0 +1,13 @@
+package com.member.invitation.memberinvitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberInvitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberInvitationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/member/invitation/memberinvitation/MemberInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.member.invitation.memberinvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberInvitationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
